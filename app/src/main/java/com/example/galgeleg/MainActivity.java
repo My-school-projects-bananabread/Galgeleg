f803f6bce53d882fe0eba90a00b6e48774e56b35
tobias <tobiasobk@gmail.com>
2020-10-23 18:32:22 +0200

    Initial commit allerede noget lavet

---
+++ app/src/main/java/com/example/galgeleg/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.galgeleg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/*ting vi skal have med
+
+    starte projektet    ✓
+
+    Hovedmenu           x
+    Selve spillet       x
+        du har vundet skærmbillede     x
+        du har tab skærmbillede        x
+    Hjælp skærm         x
+    Highscore           x
+
+
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/galgeleg/MainMenu.java
@@ -0,0 +1,12 @@
+package com.example.galgeleg;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View.OnClickListener;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
