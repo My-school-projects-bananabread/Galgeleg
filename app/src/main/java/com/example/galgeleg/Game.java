42b4a9429f04aded6bc4da7a9b767978a546e2dc
tobias <tobiasobk@gmail.com>
2020-10-25 10:34:49 +0100

    It works missing: animation/hanged man, saving highscore and the other screeens

--- app/src/main/java/com/example/galgeleg/Game.java
+++ app/src/main/java/com/example/galgeleg/Game.java
@@ -16,6 +16,16 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 
+
+/* game loop
+1. enters game
+2. tries to guess word with total lives = 10
+    2.a if word correct reset game/screen and score += 1, total lives -= 2
+    2.b if out of lives show score and correct word, then exit
+        2.b.a save score to highscore place somehow
+
+*/
+
 public class Game extends AppCompatActivity implements View.OnClickListener{
 
     //layout
@@ -23,7 +33,7 @@ public class Game extends AppCompatActivity implements View.OnClickListener{
             LtrK, LtrL, LtrM, LtrN, LtrO, LtrP, LtrQ, LtrR, LtrS, LtrT,
             LtrU, LtrV, LtrW, LtrX, LtrY, LtrZ, LtrAE, LtrOE, LtrAA;
     Button returnFromGame;
-    TextView wordTxtView;
+    TextView wordTxtView, livesTxtView, scoreTxtView;
 
     //game variables
     String guessWord;
@@ -31,6 +41,8 @@ public class Game extends AppCompatActivity implements View.OnClickListener{
     char[] displayedWordArray;
     ArrayList<String> wordList;
     String triedLetters;
+    int lives;
+    int score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,22 +69,41 @@ public class Game extends AppCompatActivity implements View.OnClickListener{
         //clicked a letter on the keyboard
         if (v != returnFromGame){
 
+            //disable button get text
+            boolean guessedAWord = false;
+
             Button btn = findViewById(v.getId());
             String clickedBtnText = btn.getText().toString();
             char letter = clickedBtnText.charAt(0);
+            btn.setEnabled(false);
+            btn.setBackgroundColor(555555);
 
-            int letterPos = 0;
-            for(int i = 0; i < displayedWordArray.length; i++){
-                if(guessWord.charAt(i) == letter) {
-                    displayedWordArray[i] = letter;
-                }
+            //show correct letters
+            for(int i = 0; i < guessWord.length(); i++){
+
             }
 
-            displayedWordString = String.valueOf(displayedWordArray);
-            wordTxtView.setText(displayedWordString);
+            //wrong or correct
+            if(guessWord.indexOf(letter) >= 0 ){
 
-            //add some disabling of button thingy
-            //just make it :nonactive or something
+                displayedWordArray[guessWord.indexOf(letter)] = letter;
+                displayedWordString = String.valueOf(displayedWordArray);
+                wordTxtView.setText(displayedWordString);
+
+                if(!displayedWordString.contains("_")){
+                    score +=1;
+                    resetGame();
+                }
+
+            } else {
+                lives -= 1;
+            }
+            if (lives < 0){
+                //skal gøre så scoren bliver gemt
+                finish();
+            } else {
+                livesTxtView.setText(String.valueOf(lives));
+            }
         }
 
     }
@@ -81,6 +112,13 @@ public class Game extends AppCompatActivity implements View.OnClickListener{
         //Variables
         wordList = new ArrayList<String>();
         wordTxtView = findViewById(R.id.wordTxtView);
+
+        lives = 10;
+        livesTxtView = findViewById(R.id.livesTxtView);
+        livesTxtView.setText(String.valueOf(lives));
+        score = 0;
+        scoreTxtView = findViewById(R.id.scoreTxtView);
+        scoreTxtView.setText(String.valueOf(score));
 
         //fill up word list
         InputStream inputStream = null;
@@ -113,6 +151,10 @@ public class Game extends AppCompatActivity implements View.OnClickListener{
 
         //shuffle, get first word, remove from list
         Collections.shuffle(wordList);
+        resetGame();
+    }
+
+    public void resetGame(){
         guessWord = wordList.get(0).toUpperCase();
         wordList.remove(0);
 
@@ -122,11 +164,86 @@ public class Game extends AppCompatActivity implements View.OnClickListener{
 
         displayedWordString = String.valueOf(displayedWordArray);
         wordTxtView.setText(displayedWordString);
+        enableAllButtons();
+
+        //score and lives
+        if(score >= 10){
+            lives = 0;
+        } else {
+            lives = 10-score;
+        }
+        livesTxtView.setText(String.valueOf(lives));
+        scoreTxtView.setText(String.valueOf(score));
     }
 
     //disable back button on phone
     @Override
     public void onBackPressed() {}
+
+    public void enableAllButtons(){
+        if(true){
+            LtrA.setEnabled(true);
+            LtrB.setEnabled(true);
+            LtrC.setEnabled(true);
+            LtrD.setEnabled(true);
+            LtrE.setEnabled(true);
+            LtrF.setEnabled(true);
+            LtrG.setEnabled(true);
+            LtrH.setEnabled(true);
+            LtrI.setEnabled(true);
+            LtrJ.setEnabled(true);
+            LtrK.setEnabled(true);
+            LtrL.setEnabled(true);
+            LtrM.setEnabled(true);
+            LtrN.setEnabled(true);
+            LtrO.setEnabled(true);
+            LtrP.setEnabled(true);
+            LtrQ.setEnabled(true);
+            LtrR.setEnabled(true);
+            LtrS.setEnabled(true);
+            LtrT.setEnabled(true);
+            LtrU.setEnabled(true);
+            LtrV.setEnabled(true);
+            LtrW.setEnabled(true);
+            LtrX.setEnabled(true);
+            LtrY.setEnabled(true);
+            LtrZ.setEnabled(true);
+            LtrAE.setEnabled(true);
+            LtrOE.setEnabled(true);
+            LtrAA.setEnabled(true);
+        }
+        if(true){
+            LtrA.setBackgroundResource(R.drawable.rounded_button);
+            LtrB.setBackgroundResource(R.drawable.rounded_button);
+            LtrC.setBackgroundResource(R.drawable.rounded_button);
+            LtrD.setBackgroundResource(R.drawable.rounded_button);
+            LtrE.setBackgroundResource(R.drawable.rounded_button);
+            LtrF.setBackgroundResource(R.drawable.rounded_button);
+            LtrG.setBackgroundResource(R.drawable.rounded_button);
+            LtrH.setBackgroundResource(R.drawable.rounded_button);
+            LtrI.setBackgroundResource(R.drawable.rounded_button);
+            LtrJ.setBackgroundResource(R.drawable.rounded_button);
+            LtrK.setBackgroundResource(R.drawable.rounded_button);
+            LtrL.setBackgroundResource(R.drawable.rounded_button);
+            LtrM.setBackgroundResource(R.drawable.rounded_button);
+            LtrN.setBackgroundResource(R.drawable.rounded_button);
+            LtrO.setBackgroundResource(R.drawable.rounded_button);
+            LtrP.setBackgroundResource(R.drawable.rounded_button);
+            LtrQ.setBackgroundResource(R.drawable.rounded_button);
+            LtrR.setBackgroundResource(R.drawable.rounded_button);
+            LtrS.setBackgroundResource(R.drawable.rounded_button);
+            LtrT.setBackgroundResource(R.drawable.rounded_button);
+            LtrU.setBackgroundResource(R.drawable.rounded_button);
+            LtrV.setBackgroundResource(R.drawable.rounded_button);
+            LtrW.setBackgroundResource(R.drawable.rounded_button);
+            LtrX.setBackgroundResource(R.drawable.rounded_button);
+            LtrY.setBackgroundResource(R.drawable.rounded_button);
+            LtrZ.setBackgroundResource(R.drawable.rounded_button);
+            LtrAE.setBackgroundResource(R.drawable.rounded_button);
+            LtrOE.setBackgroundResource(R.drawable.rounded_button);
+            LtrAA.setBackgroundResource(R.drawable.rounded_button);
+        }
+    }
 
     public void declareButtons() {
         if(true){
